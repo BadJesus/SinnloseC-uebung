a40a1edee1c6aa7451edaba1d937b8c87715659d
ImSoEvil <imsoevil@imsoevil.(none)>
2013-09-04 22:47:49 +0200

    first backup

---
+++ Uebung8/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Uebung8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 160);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(319, 108);
+            this.listBox1.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(232, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Durchsuchen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(232, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(337, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Berechnen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Protokoll:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 94);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Daten im Arbeitsverzeichnis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Eingabe:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Arbeitsverzeichnis:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(337, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 29);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Ende";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 283);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Uebung8/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Uebung8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Unterprogramme zur Matrizenbearbeitung
+    public void Addition(ref double[,] c,double[,] a,double[,] b,int n,int m,double faktor)
+    {
+    // Addition c = a + factor * b
+    // n .. Zeilenlänge von c, a und b
+    // m .. Spaltenlänge von c, a und b
+      int i, j;
+      for(i = 0; i < n; i++)
+      {
+        for (j = 0; j < m; j++)
+        {
+          c[i, j] = a[i, j] + faktor * b[i, j];
+        }
+      }
+    }
+    public void Multiplikation(ref double[,] c, double[,] a, double[,] b, int n, int m, int k, int trans)
+    {
+    // Multiplikation c = a * b
+    // n .. Zeilenlänge von c und a bzw. Spaltenlänge von a
+    // k .. Spaltenlänge von c und b
+    // m .. Zeilenlänge von b und Spalten- bzw. Zeilenlänge von a
+    // trans=0  .. Produkt c = a * b
+    // trans<>0 .. Produkt c = a(t) * b
+      int i, j, l;
+      double wert;
+      for(i=0;i<n;i++)
+      {
+        for(j=0;j<k;j++)
+        {
+          c[i, j] = 0.0;
+          for(l = 0;l< m;l++)
+          {
+            if( trans == 0) wert = a[i, l];
+            else wert = a[l, i];
+            c[i, j] = c[i, j] + wert * b[l, j];
+          }
+        }
+      }
+    }
+    public void Multi_Diagonal(ref double[,] c, double[,] a, double[] b, int n, int m, int trans)
+    {
+    // Multiplikation c = a * b
+    // b ist Diagonalmatrix gegeben als Vektor mit den Elementen der Diagonalen
+    // n .. Zeilenlänge von c und Zeile- bzw. Spaltenlänge von a
+    // m .. Spaltenlänge von c und Spalten- bzw. Zeilenlänge von a
+    // trans=0 .. Produkt c = a * b
+    // trans<>0 .. Produkt c = a(t) * b
+      int i, j;
+      for (i = 0; i < n; i++)
+      {
+        for (j = 0; j < m; j++)
+        {
+          if (trans == 0) c[i, j] = a[i, j] * b[j];
+          else c[i, j] = a[j, i] * b[j];
+        }
+      }
+    }
+    public void Quadrat_Form(ref double[,] c, double[,] a, double[] b, int n, int m)
+    {
+    // Multiplikation c = a(t) * b * a
+    // b ist Diagonalmatrix gegeben als Vektor mit den Elementen der Diagonalen
+    // m .. Zeilenlänge von c und a
+    // n .. Spaltenlänge von c und a
+      int i, j, l;
+      for (i = 0; i < n; i++)
+      {
+        for (j = 0; j < n; j++)
+        {
+          c[i, j] = 0.0;
+          for (l = 0; l < m; l++)c[i, j] = c[i, j] + a[l, i] * a[l, j] * b[l];
+        }
+      }
+    }
+    public int Inverse(ref double[,] a, int n, double S, out double det, out double cond)
+    {
+    // Aufruf mit a .. Ausgangs- und Ergebnismatrix
+    //            S .. Schrankenwert für den Abbruch
+    //            det .. Determiante von a
+    //            cond .. Konditionszahl von a
+    //            return = 0 .. fehlerfrei
+    //            return = i .. Unbekannte i nicht bestimmbar
+      double ai, amax;
+      int i, j, k;
+
+      det = 0.0;
+      cond = 0.0;
+      // 1. Berechnung von G transponiert
+      if (a[0, 0] < S) return (1);
+      else
+      {
+        amax = a[0, 0];
+        a[0, 0] = Math.Sqrt(a[0, 0]);
+        det = a[0, 0];
+        for (i = 0; i < n-1; i++)
+        {
+          if (a[i + 1, i + 1] > amax) amax = a[i + 1, i + 1];
+          for (j = i + 1; j < n; j++)
+          {
+            ai = 0.0;
+            for (k = 0; k <= i-1; k++) ai = ai + a[k, i] * a[k, j];
+            a[i, j] = (a[i, j] - ai) / a[i, i];
+          }
+          ai = 0.0;
+          for (j = 0; j <= i; j++) ai = ai + a[j, i + 1] * a[j, i + 1];
+          ai = a[i + 1, i + 1] - ai;
+          if (ai < S) return (i + 2);
+          else
+          {
+            a[i + 1, i + 1] = Math.Sqrt(ai);
+            det = det * a[i + 1, i + 1];
+          }
+        }
+        det = det * det;
+      }
+      //. Berechnung von G**-1
+      cond = amax * n;
+      a[0, 0] = 1.0 / a[0, 0];
+      for (i = 0; i < n-1; i++)
+      {
+        for (j = i + 1; j < n; j++)
+        {
+          ai = 0.0;
+          for (k = i; k <= j - 1; k++) ai = ai - a[i, k] * a[k, j];
+          a[i, j] = ai / a[j, j];
+        }
+        a[i + 1, i + 1] = 1.0 / a[i + 1, i + 1];
+      }
+      // 3. Berechnung der Inversen im oberen Dreieck
+      amax = 0.0;
+      for (i = 0; i < n; i++)
+      {
+        for (j = i; j < n; j++)
+        {
+          ai = 0.0;
+          for (k = j; k < n; k++) ai = ai + a[i, k] * a[j, k];
+          a[i, j] = ai;
+        }
+        if (a[i, i] > amax) amax = a[i, i];
+      }
+      cond = cond * amax;
+      //  4. Umspeichern der Inversen in das untere Dreieck
+      for (i = 0; i < n - 1; i++)
+      {
+        for (j = i + 1; j < n; j++) a[j, i] = a[i, j];
+      }
+      return (0);
+    }
+  
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                string zeile;
+                int i = 0, j = 0, merk = 0, k, m, n;
+                double hz, v;
+                double[,] A = new double[5, 5];
+                double[,] AP = new double[5, 5];
+                double[] L = new double[4];
+                double[] P = new double[4]; //Matrix definiert die 100 Zeilen und 100 Spalten hat. 
+                System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(textBox1.Text); // Überprüfung des Arbeitsverzeichnis
+                if (!dir.Exists) // exisitert wirklich? dann...
+                {
+                    listBox1.Items.Add("Arbeitsverzeichnis existiert nicht");
+                }
+                else
+                {
+
+                    listBox1.Items.Add("Working Directory konnte geladen werden, Berechnung erfolgt.");
+                   System.IO.FileInfo fil = new System.IO.FileInfo(textBox1.Text + "\\" + textBox2.Text); // Überprüfung der Eingabedatei mit absolutem Pfad
+                    if (!fil.Exists) // existiert wirklich? dann..  ! verneint die if abfrage 
+                    {
+                        listBox1.Items.Add("Ausgabedatei konnte nicht angelegt werden.");
+
+                    }
+                    else
+                    {
+                        if (textBox3.Text.Length == 0) textBox3.Text = "prot.txt";
+                        System.IO.FileInfo fil1 = new System.IO.FileInfo(textBox1.Text + "\\" + textBox3.Text); // Überprüfung der Eingabedatei mit absolutem Pfad
+                        if (fil1.Exists) // existiert wirklich? dann..
+                        {
+                            //hier stand voher nur ne mitteilung die Datei gibt es.. allerdings habt ihr nicchts damit gemacht.. hab die also einfach mal gelöscht und den else Fall also default gsetzt.
+                            fil1.Delete();
+                            listBox1.Items.Add("Alte prot.txt Datei wurde gelöscht.");
+
+                        }
+                                       
+                
+
+                            
+                            System.IO.StreamReader ein = new System.IO.StreamReader(textBox1.Text + "\\" + textBox2.Text);
+                            System.IO.StreamWriter aus = new System.IO.StreamWriter(textBox1.Text + "\\" + textBox3.Text);
+                            char[] Tr = { ' ' };
+                            listBox1.Items.Add("Die prot.txt wurde angelegt.");
+                           
+                            // Einlesen der Matrix A, des Vektros L, der Diagonalmatrix P
+
+                            while (!ein.EndOfStream)
+                            {
+                                zeile = ein.ReadLine();
+                                i++;
+                                //zeile.Substring(0, 2);
+                                if (String.Compare(zeile.Substring(0, 2), "M=") == 0)
+                                {
+                                    if (!int.TryParse(zeile.Substring(2, zeile.Length - 2), out m)) m = 0;
+                                }
+                                if (String.Compare(zeile.Substring(0, 2), "N=") == 0)
+                                {
+                                    if (!int.TryParse(zeile.Substring(2, zeile.Length - 2), out n)) n = 0;
+                                }
+                                if (String.Compare(zeile.Substring(0, 2), "A=") == 0) merk = 1;
+                                if (String.Compare(zeile.Substring(0, 2), "L=") == 0) merk = 2;
+                                if (String.Compare(zeile.Substring(0, 2), "P=") == 0) merk = 3;
+
+                                if (merk == 1 && i > 3)
+                                {
+                                    string[] words = zeile.Split(Tr);
+                                    j = -1;
+                                    foreach (string s in words) //foreach läuft den Vektor so lange ab bis nichts mehr drin steht. 
+                                    {
+                                        j++;
+                                        if (!double.TryParse(s, out A[i - 4, j])) A[i - 4, j] = 0.0;
+                                    }
+                                }
+                                
+                                if (merk == 2)
+                                {
+                                    string[] words = zeile.Split(Tr);
+                                    j = -1;
+                                    foreach (string s in words) //foreach läuft den Vektor so lange ab bis nichts mehr drin steht. 
+                                    {
+                                        j++;
+                                        if (!double.TryParse(s, out L[j])) L[j] = 0.0;
+                                    }
+                                }
+
+                                if (merk == 3) 
+                                {
+                                    string[] words = zeile.Split(Tr);
+                                    j = -1;
+                                    foreach (string s in words) //foreach läuft den Vektor so lange ab bis nichts mehr drin steht. 
+                                    {
+                                        j++;
+                                        if (!double.TryParse(s, out P[j])) P[j] = 0.0;
+                                    }
+                                }
+                            }
+
+                            aus.WriteLine("Ausgleichung nach vermittelnden Beobachtungen");
+                            aus.WriteLine("---------------------------------------------");
+                            aus.WriteLine("");
+                            aus.WriteLine("Arbeitsverzeichnis: " + textBox1.Text);
+                            aus.WriteLine("Eingabedatei: " + textBox2.Text);
+                            aus.WriteLine("Protokolldatei: " + textBox3.Text);
+                            aus.WriteLine("Bearbeitungszeitpunkt: " + DateTime.Now);
+                            aus.WriteLine("");
+                            aus.WriteLine("Anzahl der Beobachtungen: ");
+                            aus.WriteLine("Anzahl der Unbekannten: ");
+                            aus.WriteLine("");
+                            aus.WriteLine("Koeffizientenmatrix:");
+
+                            // Konditionszahl und Determinante
+
+
+                            // Inverse des Normalgleichungssystems (A'P A)^-1
+                            Quadrat_Form(ref AP, A, P, 4, 4);
+
+                            for (i = 0; i < 3; i++) 
+                            {
+                                aus.WriteLine(AP[i, 0] + " " + AP[i, 1] + " " + AP[i, 2]);
+                                //for (j = 0; j < 3; j++)   
+                                    //aus.Write(AP[i, j]);
+                            }
+                            // Konditionszahl und Determinante
+                            // Standardabweichung der Gewichtseinheit und Freiheitsgrad
+                            // Unbekannte X mit zugehörigen Standardabweichungen
+                            // Verbesserungen v 
+                            // Redundanzen r(i)
+                            // Kontrolle A'Pv = 0
+
+
+
+                            ein.Close();
+                            aus.Close();
+                              // Einlesen von L und P (erledigt) 
+                              // Matrizenoperationen (siehe Skript)
+                              // Protokollierung in die Ausgabedatei 
+                            listBox1.Items.Add("Berechnung abgeschlossen.");
+                        }
+                    }
+                
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+
+                string datei;
+                char[] tr = { '\\' };
+                int i = 0;
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    datei = openFileDialog1.FileName;
+
+                    string[] words = datei.Split(tr);
+                    textBox1.Text = "";
+                    foreach (string s in words)
+                    {
+                        i++;
+                        if (i > 1 && i < words.Length) textBox1.Text += "\\";
+                        if (i < words.Length) textBox1.Text += s;
+                        textBox2.Text = s;
+                    }
+                }
+                listBox1.Items.Add("Working Directory wurde gesetzt.");
+               
+            }
+         
+            
+
+            }
+        //einfach wo der Fehler auftritt diese methode aufrufen ( error_message(); )
+        private void error_message()
+        {
+            listBox1.Items.Add("Protokoll wurde fehlerhaft erstellt.");
+        }
+    }
+}
+
+
+
+
+
+
+
+
